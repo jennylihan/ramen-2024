b58e54165521f4475ce342db729d3e01a2441858
jenny han <jennyhan@cs.stanford.edu>
2023-12-27 22:21:10 -0800

    change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'Ramen 2024',
+  description: 'Jenny and Jacob serve up internet ramen.',
 }
 
 export default function RootLayout({
