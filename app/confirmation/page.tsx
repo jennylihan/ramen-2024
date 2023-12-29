642671750e01c3aef3b6086cc98ded7b6113c7d1
Jacob Wolf <jacob.h.wolf@gmail.com>
2023-12-28 19:28:50 -0500

    Add receipt font, reformat receipt, change accent color

--- app/confirmation/page.tsx
+++ app/confirmation/page.tsx
@@ -1,15 +1,6 @@
 'use client'
-import localFont from '@next/font/local'
 import { useSearchParams } from 'next/navigation'
-const receipt = localFont({
-    src: [
-        {
-            path: '../assets/fake receipt.otf',
-            weight: '400'
-        },
-    ],
-    variable: '--font-receipt'
-})
+
 export default function ConfirmationPage() {
     //get search params from url using next app router  
     const searchParams = useSearchParams()
@@ -18,20 +9,21 @@ export default function ConfirmationPage() {
 
     return (
         <>
-            <div className="h-screen w-screen bg-[#e2231b] flex justify-center items-center">
-                <div className="flex flex-row items-center justify-center p-20 max-w-xl z-10 bg-white border-8 border-black">
-                    <div className={`${receipt.variable}`}>
+            <div className="min-h-screen w-screen bg-red flex flex-col justify-center items-center">
+                <div className="grow flex flex-col items-left justify-between w-64 sm:w-[500px] sm:max-h-[65vh] my-12 p-4 sm:p-8 bg-white border-8 border-black font-receipt">
+                        <div>
                         <p className="text-2xl">{`ORDER PLACED, ${orderName}!`}</p>
-                        <div className="h-9"></div>
-                        <p>{`ORDER ID: ${orderId}`}</p>
-                        <p>{`ORDER PLACED: ${(new Date).toDateString()}`}</p>
-                        <p>{`ORDER READY BY: Mon Jan 1, 2024`}</p>
-                        <div className="h-36"></div>
-                        <p>AMOUNT DUE: $0.00</p>
-                        <div className="h-24"></div>
+                        <p>ORDER ID:<br />{`${orderId}`}</p>
+                        <p>ORDER PLACED:<br />{`${(new Date).toDateString()}`}</p>
+                        <p>ORDER READY BY:<br />Mon Jan 1 2024</p>
+                        </div>
+                        <div>
+                        <p className="text-xl">AMOUNT DUE: $0.00</p>
+                        </div>
+                        <div>
                         <p>SEND TO A FRIEND!</p>
-                        <p>THANK YOU FOR SHOPPING LOCAL</p>
-                    </div>
+                        <p>THANK YOU FOR SHOPPING LOCAL!</p>
+                        </div>
                 </div>
             </div>
         </>
